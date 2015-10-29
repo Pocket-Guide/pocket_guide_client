1cfaa7b7bc91d8cde69e3a8a7ac852ebe6243f2f
山田　潤 <junyamada107@gmail.com>
2015-10-29 21:11:04 +0900

    create entry point (react and redux)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer)
+
+React.render(
+  <Provider store={store}>
+    {() => <App />}
+  </Provider>,
+  document.getElementById('root')
+);
+
