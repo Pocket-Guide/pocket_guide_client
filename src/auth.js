26b6342b02562635f1b87e3009e68235490d8f38
山田　潤 <junyamada107@gmail.com>
2015-11-13 00:18:06 +0900

    redirect to sign_up view

---
+++ src/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+  loggedIn() {
+    return !!localStorage.token
+  }
+}
--- src/index.js
+++ src/index.js
@@ -7,13 +7,20 @@ import Top from './containers/Top'
 import AuthenticationPage from './containers/AuthenticationPage'
 import rootReducer from './reducers';
 import { Router, Route, RouteHandler, IndexRoute } from 'react-router';
+import auth from './auth'
 
 const store = configureStore()
 const history = createBrowserHistory();
 
+function requireAuth(nextState, replaceState) {
+  if (!auth.loggedIn()){
+    replaceState({ nextPathname: nextState.location.pathname }, 'sign_up')
+  }
+}
+
 const routes = (
   <Route name="app" path="/" component={ App }>
-    <IndexRoute component={Top} />
+    <IndexRoute component={Top} onEnter={ requireAuth } />
     <Route path="/sign_in" component={ AuthenticationPage } />
     <Route path="/sign_up" component={ AuthenticationPage } />
   </Route>
