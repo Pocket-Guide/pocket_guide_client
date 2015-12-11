6889847f6f2daa7e17ef3965eb1c3a9b9ddf80d8
山田　潤 <junyamada107@gmail.com>
2015-12-11 22:01:58 +0900

    update index.js and reducer/index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 import App from './containers/App';
-import Top from './containers/Top'
+import TopPage from './containers/TopPage'
+import Home from './containers/Home'
+import UserOnly from './containers/UserOnly'
+import QuestionsPage from './containers/QuestionsPage'
 import AuthenticationPage from './containers/AuthenticationPage'
+import MakeTourPage from './containers/MakeTourPage'
 import rootReducer from './reducers';
 import { Router, Route, RouteHandler, IndexRoute } from 'react-router';
 import auth from './auth'
@@ -14,7 +18,12 @@ const history = createBrowserHistory();
 
 const routes = (
   <Route component={ App }>
-    <Route path="/" component={Top} />
+    <Route component={UserOnly} >
+      <Route path="/" component={Home} />
+      <Route path="/tours" component={MakeTourPage} />
+      <Route path="/questions" component={QuestionsPage} />
+    </Route>
+    <Route path="/top" component={TopPage} />
     <Route path="/sign_in" component={ AuthenticationPage } />
     <Route path="/sign_up" component={ AuthenticationPage } />
   </Route>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,17 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
-import current_user from './current_user'
-
-const initialState = {}
+import current_user from './current_user';
+import questions from './questions';
+import tour from './tour';
+import step from './step';
+import answers from './answers';
 
 const rootReducer = combineReducers({
+  tour,
+  questions,
   current_user,
+  step,
+  answers,
   form
 });
 
